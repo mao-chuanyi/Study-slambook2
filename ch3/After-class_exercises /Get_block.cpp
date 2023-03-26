f54a322352f45bc0e79e806b5f79fd19a4ce1f03
chuanzhang <maochuanyi@outlook.com>
2023-03-26 20:27:39 +0800

    execercise

---
+++ ch3/After-class_exercises /Get_block.cpp
@@ -0,0 +1,16 @@
+//
+// Created by mao on 3/26/23.
+//
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+int main(){
+    MatrixXd A = MatrixXd::Random(10, 10);
+    Matrix3d I = Matrix3d::Identity();
+    I = A.block<3,3>(A.cols() - 3, A.rows() - 3);
+    cout << "A = \n" << A << endl;
+    cout << "I = \n" << I << endl;
+ }
