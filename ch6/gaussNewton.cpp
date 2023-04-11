76220ba5650fac012f8544eb06270f7c1ceeaa3d
mao <maochuanyi@outlook.com>
2023-04-11 14:50:42 +0800

    mend bug

--- ch6/gaussNewton.cpp
+++ ch6/gaussNewton.cpp
@@ -1,6 +1,5 @@
-//
 // Created by mao on 23-4-10.
-//
+// This program uses Gauss-Newton method to fit a curve
 #include <iostream>
 #include <opencv4/opencv2/opencv.hpp>
 #include <eigen3/Eigen/Core>
@@ -11,69 +10,75 @@ using namespace Eigen;
 
 int main (int argc, char **argv)
 {
-    double ar = 1.0, br = 2.0, cr = 1.0;
-    double ae = 2.0, be = -1.0, ce = 5.0;
+    // true parameters
+    double a_true = 1.0, b_true = 2.0, c_true = 1.0;
+    // estimated parameters
+    double a_est = 2.0, b_est = -1.0, c_est = 5.0;
+    // number of data points
     int N = 100;
-    double w_sigma = 1.0; //噪声sigma值
+    // noise sigma
+    double w_sigma = 1.0;
     double inv_sigma = 1.0 / w_sigma;
-    cv::RNG rng;
-    
-    vector<double> x_data, y_data;
+    cv::RNG rng; // random number generator
+
+    vector<double> x_data, y_data; // data vectors
     for (int i = 0; i < N; i++)
     {
         double x = i / 100.0;
         x_data.push_back(x);
-        y_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+        // generate noisy y data
+        y_data.push_back(exp(a_true * x * x + b_true * x + c_true) + rng.gaussian(w_sigma * w_sigma));
     }
-    //开始迭代
-    int iterations = 100;//迭代次数
+    // start iteration
+    int iterations = 100;// iteration times
     double cost = 0, lastCost = 0;
 
     chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
     for (int iter = 0; iter < iterations; iter++)
     {
-        Matrix3d H = Matrix3d::Zero(); //Hessian
-        Vector3d b = Vector3d::Zero(); //bias
+        Matrix3d H = Matrix3d::Zero(); // Hessian matrix
+        Vector3d b = Vector3d::Zero(); // bias vector
         cost = 0;
 
         for (int i = 0; i < N; i++)
         {
-            double xi = x_data[i], yi = y_data[i]; //第i个数据点
-            double error = xi - exp(ae * xi * xi + be * xi + ce); //误差
-            Vector3d J;//雅可比
-            J[0] = -xi * xi * exp(ae * xi * xi + be * xi + ce);
-            J[1] = -xi * exp(ae * xi * xi + be * xi + ce);
-            J[2] = exp(ae * xi * xi + be * xi + ce);
+            double xi = x_data[i], yi = y_data[i]; // the i-th data point
+            double error = yi - exp(a_est * xi * xi + b_est * xi + c_est); // error
+            Vector3d J;// Jacobian matrix
+            J[0] = -xi * xi * exp(a_est * xi * xi + b_est * xi + c_est); // derivative of error w.r.t a_est
+            J[1] = -xi * exp(a_est * xi * xi + b_est * xi + c_est); // derivative of error w.r.t b_est
+            J[2] = -exp(a_est * xi * xi + b_est * xi + c_est); // derivative of error w.r.t c_est
 
             H += inv_sigma * inv_sigma * J * J.transpose();
             b += -inv_sigma * inv_sigma * error * J;
 
             cost += error * error;
         }
-        //求解Hx=b
-        Vector3d dx = H.ldlt().solve(b);//对于正定矩阵，使用cholesky分解来解方程
+        // solve Hx=b
+        Vector3d dx = H.ldlt().solve(b);// use cholesky decomposition to solve the equation for positive definite matrix
         if (isnan(dx[0]))
         {
             cout << "result is nan!" << endl;
             break;
         }
-        if (iter > 0 && iter >= lastCost)
+        if (iter > 0 && cost >= lastCost)
         {
             cout << "cost:" << cost << ">=lastCost" << ",break" << endl;
             break;
         }
-        ae += dx[0];
-        be += dx[1];
-        ce += dx[2];
+        a_est += dx[0];
+        b_est += dx[1];
+        c_est += dx[2];
 
         lastCost = cost;
-        cout << "total cost: " << cost << ",\t\tupdate: " << dx.transpose() << "\t\testimated params: " << ae << "," << be << "," << ce << endl;
-
+        cout << "total cost: " << cost <<
+             ",\t\testimated params: " << a_est << "," << b_est << "," << c_est << endl;
     }
     chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-    cout << "solve time cost = " << time_used.count() << "seconds." << endl;
-    cout << "estimated abc = " << ae << ", " << be << ", " << ce << endl;
+    chrono::duration<double> time_used =
+            chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+    cout << "solve time cost: " << time_used.count() << " seconds." << endl;
 
+    cout << "estimated abc: " << a_est << ", " << b_est << ", " << c_est << endl;
     return 0;
 }
